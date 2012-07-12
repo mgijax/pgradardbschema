c00df1e0eab11d00839f962a59e0d51314f1d7cd
Lori Corbani <Lori.Corbani@jax.org>
2012-07-12 12:54:10 +0000

    pgradardbschema-5-0-0-1

---
+++ objectCounter.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Count the number of create scripts for each type of object in the schema
+# product and the number of objects that will be created by those scripts.
+
+cd `dirname $0`
+TOP=`pwd`
+
+echo "Object Type     Count"
+echo "==============  ============"
+
+cd ${TOP}/table
+echo "Tables          `ls *_create.object | wc -l` scripts"
+
+cd ${TOP}/index
+echo "Indexes         `ls *_create.object | wc -l` scripts  (`grep -i '^create .* index ' *_create.object | wc -l` indexes)"
+
+cd ${TOP}/key
+echo "Keys            `ls *_create.object | wc -l` scripts  (`grep -i '^sp_primarykey ' *_create.object | wc -l` primary keys, `grep -i '^sp_foreignkey ' *_create.object | wc -l` foreign keys)"
+
+cd ${TOP}/trigger
+echo "Triggers        `ls *_create.object | wc -l` scripts  (`grep -i '^create trigger ' *_create.object | wc -l` triggers)"
+
+cd ${TOP}/procedure
+echo "Procedures      `ls *_create.object | wc -l` scripts"
+
---
+++ reconfig.csh
@@ -0,0 +1,20 @@
+#!/bin/csh -x -f
+
+#
+# Drop and re-create database triggers, stored procedures and views
+#
+#
+
+cd `dirname $0` && source ./Configuration
+
+rm -rf */logs/*/*log
+./default/default_unbind.csh
+./default/default_bind.csh
+./key/key_drop.csh
+./key/key_create.csh
+./index/index_drop.csh
+./index/index_create.csh
+./procedure/procedure_drop.csh
+./procedure/procedure_create.csh
+./trigger/trigger_drop.csh
+./trigger/trigger_create.csh
