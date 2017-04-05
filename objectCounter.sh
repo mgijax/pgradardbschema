6c895820e19155ce10dc23604dd3c40c983f9f9f
Lori Corbani <lori.corbani@jax.org>
2017-04-05 14:25:05 -0400

    TR12083/varchars to text

--- objectCounter.sh
+++ objectCounter.sh
@@ -5,6 +5,7 @@
 
 cd `dirname $0`
 TOP=`pwd`
+unset LC_ALL
 
 echo "Object Type     Count"
 echo "==============  ============"
@@ -13,13 +14,25 @@ cd ${TOP}/table
 echo "Tables          `ls *_create.object | wc -l` scripts"
 psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_catalog.pg_tables where schemaname = 'radar'"
 
+cd ${TOP}/key
+echo "Primary Keys         `ls *_create.object | wc -l` scripts  (`grep -i 'ADD PRIMARY KEY' *_create.object | wc -l` primary keys)"
+psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_stat_user_indexes where schemaname = 'radar' and indexrelname like '%_pkey'" 
+
 cd ${TOP}/index
-echo "Indexes         `ls *_create.object | wc -l` scripts  (`grep -i '^create .*index ' *_create.object | wc -l` indexes)"
-psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_stat_user_indexes where schemaname = 'radar' and indexrelname like '%_idx%'" 
+echo "Indexes         `ls *_create.object | wc -l` scripts  (`grep -i '^create' *_create.object | wc -l` indexes)"
+psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_stat_user_indexes where schemaname = 'radar' and indexrelname not like '%_pkey'" 
 
 cd ${TOP}/procedure
 echo "Procedures      `ls *_create.object | wc -l` scripts (`grep -i '^CREATE OR REPLACE' *_create.object | wc -l` functios)"
-psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "\df" | grep -i 'normal' | wc -l
+psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "\df" | grep -i 'normal' | grep -i 'radar' | wc -l
+
+#cd ${TOP}/trigger
+#echo "Triggers        `ls *_create.object | wc -l` scripts  (`grep -i '^create trigger ' *_create.object | wc -l` triggers)"
+#psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_catalog.pg_trigger where tgname like '%_trigger'" 
+
+#cd ${TOP}/view
+#echo "Views           `ls *_create.object | wc -l` scripts"
+#psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_catalog.pg_views where schemaname = 'radar'" 
 
 cd ${TOP}/key
 echo "Keys            `ls *_create.object | wc -l` scripts  (`grep -i 'ADD PRIMARY' *_create.object | wc -l` primary keys)"
